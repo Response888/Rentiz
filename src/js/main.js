53377f59ef62fae36a14d021bda0e756d91eb360
vadimkuznezov <kuznecovvadim907@gmail.com>
2022-11-19 20:18:32 +0600

    init:creat

--- src/js/main.js
+++ src/js/main.js
@@ -16,3 +16,15 @@ if (filter) {
     })
   })
 }
+
+const swiper = new Swiper(".swiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  slidesPerGroup: 3,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
